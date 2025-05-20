7ef4fee872fb349ec989559ba64356e520ec48a2
Iqra Khaliq <114660239+Iqra562@users.noreply.github.com>
2025-05-20 17:11:12 +0500

    basic crud

--- 02_express/index.js
+++ 02_express/index.js
@@ -16,6 +16,43 @@ app.post('/sendData',(req,res)=>{
 
 })
 
+app.get('/getData',(req,res)=>{
+    res.status(200).send(data);
+})
+app.get('/data/:id',(req,res)=>{
+ const element=data.find(element => element.id === parseInt(req.params.id)); //anything which comes from params it will be in string format
+ if(!element){
+    return res.status(404).send('data not found')
+ }
+ res.status(200).send(element)
+
+
+})
+
+app.put('/updateData/:id',(req,res)=>{
+  const element=data.find(element => element.id === parseInt(req.params.id));  
+ if(!element){
+    return res.status(404).send('data not found')
+ }
+ const {name,price}  =req.body
+ element.name = name
+ element.price = price
+ res.send(200).send(element)
+
+})
+
+app.delete('/deleteData/:id',(req,res)=>{
+    const index = data.findIndex(t =>t.id === parseInt(req.params.id))
+    if(index === -1){
+        return res.status(404).send('data not found to delte');
+
+    }
+    data.splice(index,1)
+    return  res.status(204).send('data deleted');
+
+
+})
+
 app.get('/',(req,res)=>{
     res.send('hello from express')
 
